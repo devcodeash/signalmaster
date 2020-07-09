ff60106cf3b7ce411e24eb8997b40cad0feb31a5
Ashwin <devcodeash@gmail.com>
2020-07-09 13:10:17 +0530

    emit turn servers

--- sockets.js
+++ sockets.js
@@ -135,25 +135,26 @@ module.exports = function (server, config, hooks) {
         // tell client about stun and turn servers and generate nonces
         client.emit('stunservers', config.stunservers || []);
 
-        // create shared secret nonces for TURN authentication
-        // the process is described in draft-uberti-behave-turn-rest
-        var credentials = [];
-        // allow selectively vending turn credentials based on origin.
-        var origin = client.handshake.headers.origin;
-        if (!config.turnorigins || config.turnorigins.indexOf(origin) !== -1) {
-            config.turnservers.forEach(function (server) {
-                var hmac = crypto.createHmac('sha1', server.secret);
-                // default to 86400 seconds timeout unless specified
-                var username = Math.floor(new Date().getTime() / 1000) + (parseInt(server.expiry || 86400, 10)) + "";
-                hmac.update(username);
-                credentials.push({
-                    username: username,
-                    credential: hmac.digest('base64'),
-                    urls: server.urls || server.url
-                });
-            });
-        }
-        client.emit('turnservers', credentials);
+        // // create shared secret nonces for TURN authentication
+        // // the process is described in draft-uberti-behave-turn-rest
+        // var credentials = [];
+        // // allow selectively vending turn credentials based on origin.
+        // var origin = client.handshake.headers.origin;
+        // if (!config.turnorigins || config.turnorigins.indexOf(origin) !== -1) {
+        //     config.turnservers.forEach(function (server) {
+        //         var hmac = crypto.createHmac('sha1', server.secret);
+        //         // default to 86400 seconds timeout unless specified
+        //         var username = Math.floor(new Date().getTime() / 1000) + (parseInt(server.expiry || 86400, 10)) + "";
+        //         hmac.update(username);
+        //         credentials.push({
+        //             username: username,
+        //             credential: hmac.digest('base64'),
+        //             urls: server.urls || server.url
+        //         });
+        //     });
+        // }
+        // client.emit('turnservers', credentials);
+        client.emit('turnservers', config.turnservers || []);
     });
 
 
